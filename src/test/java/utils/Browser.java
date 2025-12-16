c6eb73533f1e155f6a58ffbabc21f79a7b26c7b3
Nikita Shinov <n.shinov@a1qa.com>
2025-12-16 23:05:12 +0100

    rolled back remoteWebDriver change

--- src/test/java/utils/Browser.java
+++ src/test/java/utils/Browser.java
@@ -1,18 +1,35 @@
 package utils;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 public class Browser {
-    private static WebDriver driverInstance = null;
+    private static RemoteWebDriver driverInstance = null;
 
     public static WebDriver getInstance() {
         if (driverInstance == null) {
+            HashMap<String, Object> chromePrefs = new HashMap<>();
+            chromePrefs.put("download.prompt_for_download", false);
+
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless", "--disable-gpu");
-            driverInstance = new ChromeDriver(options);
-            driverInstance.manage().window().maximize();
+            options.setExperimentalOption("prefs", chromePrefs);
+
+            options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
+
+            String remoteUrl = System.getenv()
+                    .getOrDefault("SELENIUM_REMOTE_URL", "http://selenium-chrome:4444/wd/hub");
+
+            try {
+                driverInstance = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+
         }
         return driverInstance;
     }
